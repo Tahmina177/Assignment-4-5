a161312bd28926f2560a09b6f0abed41466ac3f4
Tahmina177 <sumime295@gmail.com>
2025-10-30 01:04:47 +0600

    Update q6.dart

--- Assign 04/q6.dart
+++ Assign 04/q6.dart
@@ -1,15 +1,16 @@
 void main() {
   Map<String, dynamic> personalData = {
-    "name": "mozid",
-    "address": "londoni road,pathantula",
-    "age": 24,
-    "country": "Bangladesh",
+    "name": "SUMI",
+    "address": "Tarling Street,East London",
+    "age": 22,
+    "country": "UK",
   };
   print("Initial Map: $personalData");
-  personalData["country"] = "Bangladesh";
+  personalData["country"] = "UK";
   print("\nMap after updating country: $personalData");
   print("\nPrinting Keys and Values:");
   for (String key in personalData.keys) {
     print("Key: $key, Value: ${personalData[key]}");
   }
+
 }
